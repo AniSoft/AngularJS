c46cf3851aa970f389a9b07d26d401dddacf35b3
Anita <anitanikolova@yahoo.com>
2015-01-02 23:12:21 +0200

    js/controllers/MainController.js

--- Client/js/controllers/MainController.js
+++ Client/js/controllers/MainController.js
@@ -1,3 +1,5 @@
-softUni.controller('SoftUniController',function($scope){
-	$scope.name='name';
+softUni.controller('SoftUniController',function($scope,mainData){
+	mainData.getAllAds(function(resp){
+		$scope.data=resp;
+	})
 })
