7d90e7e377afd1c93195bb8097712ffc80f29e58
Anita <anitanikolova@yahoo.com>
2015-01-02 19:58:28 +0200

    app.js

--- Client/js/app.js
+++ Client/js/app.js
@@ -1,1 +1,11 @@
-'use strict';
+var softUni=angular.module('softUniModule',['ngRoute'])
+.config(function($routeProvider){
+	$routeProvider.when('/register',{
+		templateUrl:'templates/register.html'//,
+		//controller:'SoftUniController'
+	});
+	$routeProvider.when('/ads',{
+		templateUrl:'templates/all-ads.html'
+	})
+
+});
