fa87e261367bde5fe9e6e572b93378bf1896a437
Anita <anitanikolova@yahoo.com>
2015-01-07 00:48:23 +0200

    js/services/data/towns.js

---
+++ Client/js/services/data/towns.js
@@ -0,0 +1,10 @@
+app.factory('townsData', ['$resource','baseServiceUrl',function ($resource,baseServiceUrl) {
+	var resource=$resource(baseServiceUrl+'towns');
+
+	function getAllTowns(){
+		return $resource.query();
+	}
+	return {
+		getTowns: getAllTowns
+	};
+}]);
