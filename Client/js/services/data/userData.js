fa2ca3fb325c733df2a611d695a0e991e23db299
Anita <anitanikolova@yahoo.com>
2015-01-07 00:48:46 +0200

    js/services/data/userData.js

---
+++ Client/js/services/data/userData.js
@@ -0,0 +1,39 @@
+app.factory('userData', ['$resource','baseServiceUrl','authentication',function ($resource,baseServiceUrl,authentication) {
+	
+	function registerUser(user){
+		return $resource(baseServiceUrl+'user/register')
+			.save(user)
+			.$promise
+			.then(function(data){
+				authentication.saveUser(data);
+				//console.log(authentication.getHeaders());
+			});
+	}
+
+	function loginUser(user){
+		return $resource(baseServiceUrl+'user/login')
+			.save(user)
+			.$promise
+			.then(function(data){
+				authentication.saveUser(data);
+				
+			});
+	}
+
+	function logoutUser(){
+		return $resource(baseServiceUrl+'user/logout')
+			.save(user)
+			.$promise
+			.then(function(data){
+				authentication.removeUser();
+				
+			});
+	}
+
+
+	return {
+		register:registerUser,
+		login:loginUser,
+		logout:logoutUser
+	};
+}])
