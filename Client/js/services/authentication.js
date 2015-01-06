6d22404bf10f6169d9aa19c42ed5343928f441bf
Anita <anitanikolova@yahoo.com>
2015-01-07 00:47:45 +0200

    js/services/authentication.js

---
+++ Client/js/services/authentication.js
@@ -0,0 +1,40 @@
+app.factory('authentication', function () {
+	var key='user';
+
+	function saveUserData(data){
+		localStorage.setItem(key,angular.toJson(data));
+	}
+
+	function getUserData(data){
+		return angular.fromJson(localStorage.getItem(key));
+	}	
+
+	function getHeaders(){
+		var headers={};
+		var userData=getUserData();
+
+		if (userData){
+			headers.Authorization='Bearer '+ getUserData().access_token; 	
+		};
+
+		return headers;
+		
+	}	
+
+	function removeUser(){
+		 localStorage.removeItem(key);
+	}
+
+	function isAdmin(){
+		var isAdmin= getUserData().isAdmin;
+		return isAdmin;
+	}
+
+	return {
+		saveUser:saveUserData,
+		getUser:getUserData,
+		getHeaders:getHeaders,
+		removeUser:removeUser,
+		isAdmin:isAdmin
+	};
+});  
