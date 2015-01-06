480c612a8883a5d4592c5c177a2c4103264d87a7
Anita <anitanikolova@yahoo.com>
2015-01-06 03:53:50 +0200

    js/directives/left-sidebar.js

---
+++ Client/js/directives/left-sidebar.js
@@ -0,0 +1,8 @@
+app.directive('leftPublicSidebar', function () {
+	return {
+		controller:'HomeCtrl',
+		restrict: 'E',
+		templateUrl: 'templates/public/left-sidebar.html',
+		replace: true,
+	};
+});
