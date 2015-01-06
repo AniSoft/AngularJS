d62e90cc0244860f528ddb40befc85ae593c991e
Anita <anitanikolova@yahoo.com>
2015-01-07 00:45:37 +0200

    js/controllers/LoginCtrl.js

--- Client/js/controllers/LoginCtrl.js
+++ Client/js/controllers/LoginCtrl.js
@@ -1,4 +1,4 @@
-app.controller('LoginCtrl', ['$scope', function ($scope) {
+app.controller('LoginCtrl', ['$scope','userData', function ($scope,userData) {
 		$scope.login=function(user){
 			userData.login(user);
 	}	
