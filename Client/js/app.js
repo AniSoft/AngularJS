e6ebf7e3b50fe63e0650cb4d150444509ca7c327
Anita <anitanikolova@yahoo.com>
2015-01-06 02:33:30 +0200

    js/app.js

---
+++ Client/js/app.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var app=angular.module('adsApp',['ngRoute']);
+
+app.config(['$routeProvider',function($routeProvider){
+	$routeProvider.when('/',{
+		templateUrl: 'templates/home.html',
+		controller: 'HomeCtrl'
+	} )
+}])
