4362900b10879be17b9eb3b472d4d6ec715cfaf3
Anita <anitanikolova@yahoo.com>
2015-01-07 03:08:26 +0200

    del

--- Client-1/js/controllers/AllAdsController.js
+++
@@ -1,9 +0,0 @@
-app.controller('AllAdsController', function($scope, adsData, $log) {
-	adsData.getAll()
-		.$promise
-		.then(function (data) {
-			$scope.data = data;
-		}, function (error) {
-			$log.error(error);
-		})
-});
