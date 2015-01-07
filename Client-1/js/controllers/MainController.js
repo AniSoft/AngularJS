a8b11c714c9b9af37c70e25edb8daff024cca76d
Anita <anitanikolova@yahoo.com>
2015-01-07 03:05:13 +0200

    libs

---
+++ Client-1/js/controllers/MainController.js
@@ -0,0 +1,13 @@
+softUni.controller('SoftUniController',function($scope,mainData){
+	mainData.getAllAds(function(resp){
+		$scope.data=resp;
+	});
+
+	mainData.getAllTowns(function(resp){
+		$scope.towns=resp;
+	});
+
+	mainData.getAllCategories(function(resp){
+		$scope.categories=resp;
+	});
+})
