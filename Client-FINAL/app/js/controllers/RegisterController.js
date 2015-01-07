7a1d63b83087b18ea4f015243f95cb72794a7230
Anita <anitanikolova@yahoo.com>
2015-01-07 15:10:30 +0200

    app/js/controllers/RegisterController.js

--- Client-FINAL/app/js/controllers/RegisterController.js
+++ Client-FINAL/app/js/controllers/RegisterController.js
@@ -5,19 +5,19 @@ app.controller('RegisterController',
     function ($scope, $location, townsService, authService, notifyService) {
         $scope.userData = {townId: null};
 
-        // TODO: load the towns in $scope (for the towns drop-down list)
+        $scope.userData = {townId: null};
+        $scope.towns = townsService.getTowns();
 
         $scope.register = function(userData) {
             authService.register(userData,
                 function success() {
-                    // TODO: display success message
-                    // TODO: redirect to login screen
+                    notifyService.showInfo("User registered successfully");
+                    $location.path("/");
                 },
                 function error(err) {
                     notifyService.showError("User registration failed", err);
                 }
             );
         };
-
     }
 );
