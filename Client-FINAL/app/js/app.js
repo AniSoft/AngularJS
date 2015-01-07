b3dca122eb53671461c41034fec6be5d6c9bec72
Anita <anitanikolova@yahoo.com>
2015-01-07 16:17:47 +0200

    app/js/app.js

--- Client-FINAL/app/js/app.js
+++ Client-FINAL/app/js/app.js
@@ -27,6 +27,11 @@ app.config(function ($routeProvider) {
         controller: 'UserPublishNewAdController'
     });
 
+    $routeProvider.when('/user/ads', {
+        templateUrl: 'templates/home.html',
+        controller: 'UserAdsController'
+    });
+
     $routeProvider.otherwise(
         { redirectTo: '/' }
     );
