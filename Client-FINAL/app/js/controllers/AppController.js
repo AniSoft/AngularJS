766e3ce1a5b00a2187f966e95f05607654cbb881
Anita <anitanikolova@yahoo.com>
2015-01-07 14:49:47 +0200

    app/js/controllers/AppController.js

--- Client-FINAL/app/js/controllers/AppController.js
+++ Client-FINAL/app/js/controllers/AppController.js
@@ -2,8 +2,14 @@
 
 // The AppController holds the presentation logic for the entire app (common for all screens)
 app.controller('AppController',
-    function ($scope, authService) {
-      // Put the authService in the $scope to make it accessible from all screens
-      $scope.authService = authService;
+    function ($scope, $location, authService, notifyService) {
+		// Put the authService in the $scope to make it accessible from all screens
+		$scope.authService = authService;
+        
+        $scope.logout = function() {
+            authService.logout();
+            notifyService.showInfo("Logout successful");
+            $location.path('/');
+        };
     }
 );
