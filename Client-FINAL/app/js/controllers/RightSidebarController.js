0454354e452545c4720b87a648d86306df765aed
Anita <anitanikolova@yahoo.com>
2015-01-07 13:59:31 +0200

    app/js/controllers/RightSidebarController.js

--- Client-FINAL/app/js/controllers/RightSidebarController.js
+++ Client-FINAL/app/js/controllers/RightSidebarController.js
@@ -2,8 +2,18 @@
 
 // The RightSidebarController controls the content displayed in the right sidebar
 app.controller('RightSidebarController',
-    function ($scope, categoriesService, townsService) {
+    function ($scope, $rootScope, categoriesService, townsService) {
         $scope.categories = categoriesService.getCategories();
         $scope.towns = townsService.getTowns();
+
+        $scope.categoryClicked = function(clickedCategoryId) {
+            $scope.selectedCategoryId = clickedCategoryId;
+            $rootScope.$broadcast("categorySelectionChanged", clickedCategoryId);
+        };
+
+        $scope.townClicked = function(clickedTownId) {
+            $scope.selectedTownId = clickedTownId;
+            $rootScope.$broadcast("townSelectionChanged", clickedTownId);
+        };
     }
 );
