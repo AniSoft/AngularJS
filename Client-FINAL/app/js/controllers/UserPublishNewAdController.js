3c9127826a8cf771415b4a9d7867efa5b1aee95c
Anita <anitanikolova@yahoo.com>
2015-01-07 15:31:42 +0200

    app/js/controllers/UserPublishNewAdController.js

--- Client-FINAL/app/js/controllers/UserPublishNewAdController.js
+++ Client-FINAL/app/js/controllers/UserPublishNewAdController.js
@@ -2,20 +2,35 @@
 
 app.controller('UserPublishNewAdController',
   function ($scope, $location, townsService, categoriesService,
-            userService, notifyService) {
-      $scope.adData = {townId: null, categoryId: null};
-      $scope.categories = categoriesService.getCategories();
-      $scope.towns = townsService.getTowns();
-      $scope.publishAd = function(adData) {
-          userService.createNewAd(adData,
-              function success() {
-                  // TODO: show “success” info message
-                  $location.path("/user/ads");
-              },
-              function error(err) {
-                  // TODO: show error message
-              }
-          );
-      };
+    userService, notifyService) {
+    $scope.adData = {townId: null, categoryId: null};
+    $scope.categories = categoriesService.getCategories();
+    $scope.towns = townsService.getTowns();
+    $scope.publishAd = function(adData) {
+      userService.createNewAd(adData,
+        function success() {
+        // TODO: show “success” info message
+          $location.path("/user/ads");
+        },
+        function error(err) {
+          // TODO: show error message
+        }
+        );
+    };
+
+    $scope.fileSelected = function(fileInputField) {
+      delete $scope.adData.imageDataUrl;
+      var file = fileInputField.files[0];
+      if (file.type.match(/image\/.*/)) {
+        var reader = new FileReader();
+        reader.onload = function() {
+          $scope.adData.imageDataUrl = reader.result;
+          $(".image-box").html("<img src='" + reader.result + "'>");
+        };
+        reader.readAsDataURL(file);
+      } else {
+        $(".image-box").html("<p>File type not supported!</p>");
+      }
+    };
   }
 );
