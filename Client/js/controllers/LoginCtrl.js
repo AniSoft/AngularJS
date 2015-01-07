626a4bf0c32b531201007109ae5251b054d43cc9
Anita <anitanikolova@yahoo.com>
2015-01-07 04:40:41 +0200

    js/controllers/LoginCtrl.js

--- Client/js/controllers/LoginCtrl.js
+++ Client/js/controllers/LoginCtrl.js
@@ -1,6 +1,6 @@
 app.controller('LoginCtrl', ['$scope','$location','userData', function ($scope,location,userData) {
 		$scope.login=function(user){
-			userData.login(user);
+			userData.login(user)
 				.$promise
 				.then(function(data){
 					$location.path('/');
