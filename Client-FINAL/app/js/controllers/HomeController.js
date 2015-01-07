fde3c5f38edb6c2a296022836c64f9b205892a3b
Anita <anitanikolova@yahoo.com>
2015-01-07 14:05:18 +0200

    app/js/controllers/HomeController.js

--- Client-FINAL/app/js/controllers/HomeController.js
+++ Client-FINAL/app/js/controllers/HomeController.js
@@ -2,9 +2,7 @@
 
 // The HomeController holds the presentation logic for the home screen
 app.controller('HomeController',
-    function ($scope, $rootScope, adsService, notifyService, pageSize) {
-        $rootScope.pageTitle = "Home";
-        $rootScope.showRightSidebar = true;
+    function ($scope, adsService, notifyService, pageSize) {
 
         $scope.adsParams = {
             'startPage' : 1,
