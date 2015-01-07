355422f137233a24dd2744d6a5baa4f6c4f572a1
Anita <anitanikolova@yahoo.com>
2015-01-07 13:28:46 +0200

    app/js/controllers/HomeController.js

--- Client-FINAL/app/js/controllers/HomeController.js
+++ Client-FINAL/app/js/controllers/HomeController.js
@@ -3,14 +3,26 @@
 // The HomeController holds the presentation logic for the home screen
 app.controller('HomeController',
     function ($scope, $rootScope, adsService, notifyService, pageSize) {
-      adsService.getAds(
-            null,
-            function success(data) {
-                $scope.ads = data;
-            },
-            function error(err) {
-                notifyService.showError("Cannot load ads", err);
-            }
-        );
+        $rootScope.pageTitle = "Home";
+        $rootScope.showRightSidebar = true;
+
+        $scope.adsParams = {
+            'startPage' : 1,
+            'pageSize' : pageSize
+        };
+
+        $scope.reloadAds = function() {
+            adsService.getAds(
+                $scope.adsParams,
+                function success(data) {
+                    $scope.ads = data;
+                },
+                function error(err) {
+                    notifyService.showError("Cannot load ads", err);
+                }
+            );
+        };
+
+        $scope.reloadAds();
    }
 );
