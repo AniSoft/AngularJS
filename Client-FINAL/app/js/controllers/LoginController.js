10c65dc3949361de7b4f6596b9f6f16939c61c12
Anita <anitanikolova@yahoo.com>
2015-01-07 14:38:42 +0200

    app/js/controllers/LoginController.js

--- Client-FINAL/app/js/controllers/LoginController.js
+++ Client-FINAL/app/js/controllers/LoginController.js
@@ -3,6 +3,16 @@
 // The LoginController is responsible for the "Login" screen
 app.controller('LoginController',
     function ($scope, $location, authService, notifyService) {
-        // TODO
+        $scope.login = function(userData) {
+            authService.login(userData,
+                function success() {
+                    notifyService.showInfo("Login successful");
+                    $location.path("/");
+                },
+                function error(err) {
+                    notifyService.showError("Login failed", err);
+                }
+            );
+        };
     }
 );
