b60c3329da4a5eccefc1763945a9aa5073da1dfd
Anita <anitanikolova@yahoo.com>
2015-01-07 02:30:38 +0200

    controllers/PublicAdsCtrl.js

--- Client/js/controllers/PublicAdsCtrl.js
+++ Client/js/controllers/PublicAdsCtrl.js
@@ -1,7 +1,9 @@
 app.controller('PublicAdsCtrl', ['$scope','adsData', function($scope,adsData){
+	$scope.ready=false;
 	adsData.getPublicAds()
 		.$promise
 		.then(function(data){
 			$scope.adsData=data;
+			$scope.ready=true;
 		})
 }])
