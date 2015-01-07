4ef758f9fd03d013f81d4c0ac0b2d70e639dd512
Anita <anitanikolova@yahoo.com>
2015-01-07 04:40:57 +0200

    js/services/authentication.js

--- Client/js/services/authentication.js
+++ Client/js/services/authentication.js
@@ -18,7 +18,6 @@ app.factory('authentication', function () {
 		};
 
 		return headers;
-		
 	}	
 
 	function removeUser(){
@@ -30,11 +29,16 @@ app.factory('authentication', function () {
 		return isAdmin;
 	}
 
+	function isLoggedIn(){
+		return !!getUserData(); 
+	}
+
 	return {
 		saveUser:saveUserData,
 		getUser:getUserData,
 		getHeaders:getHeaders,
 		removeUser:removeUser,
-		isAdmin:isAdmin
+		isAdmin:isAdmin,
+		isLoggedIn: isLoggedIn
 	};
 });  
