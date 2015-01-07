6a86a4c492f21f5d6c79f7cc54acd0f046f7cf90
Anita <anitanikolova@yahoo.com>
2015-01-07 14:56:33 +0200

    app/js/controllers/RegisterController.js

--- Client-FINAL/app/js/controllers/RegisterController.js
+++ Client-FINAL/app/js/controllers/RegisterController.js
@@ -3,6 +3,21 @@
 // The RegisterController is responsible for the "User Registration" screen
 app.controller('RegisterController',
     function ($scope, $location, townsService, authService, notifyService) {
-        // TODO
+        $scope.userData = {townId: null};
+
+        // TODO: load the towns in $scope (for the towns drop-down list)
+
+        $scope.register = function(userData) {
+            authService.register(userData,
+                function success() {
+                    // TODO: display success message
+                    // TODO: redirect to login screen
+                },
+                function error(err) {
+                    notifyService.showError("User registration failed", err);
+                }
+            );
+        };
+
     }
 );
