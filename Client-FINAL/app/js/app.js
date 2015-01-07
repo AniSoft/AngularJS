be0841fbed249928f5f7ed03070aecd98cdb15ac
Anita <anitanikolova@yahoo.com>
2015-01-07 13:28:30 +0200

    js/app.js

--- Client-FINAL/app/js/app.js
+++ Client-FINAL/app/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('app', ['ngRoute', 'ngResource']);
+var app = angular.module('app', ['ngRoute', 'ngResource','ui.bootstrap.pagination']);
 
 app.constant('baseServiceUrl', 'http://softuni-ads.azurewebsites.net');
 app.constant('pageSize', 2);
