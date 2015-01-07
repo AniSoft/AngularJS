f5d6c9adea0750873fcf37c55305676c2a07c5ca
Anita <anitanikolova@yahoo.com>
2015-01-07 12:45:30 +0200

    app/js/services/adsServices.js

--- Client-FINAL/app/js/services/adsServices.js
+++ Client-FINAL/app/js/services/adsServices.js
@@ -1,10 +1,20 @@
 'use strict';
 
 app.factory('adsService',
-   function ($resource, baseServiceUrl) {
-      return {
-         // TODO: implement a service to get ads
-      };
+    function ($resource, baseServiceUrl) {
+        var adsResource = $resource(
+            baseServiceUrl + '/api/ads',
+            null,
+            {
+              'getAll': {method:'GET'}
+            }
+        );
+
+        return {
+          getAds: function(params, success, error) {
+              return adsResource.getAll(params, success, error);
+          }
+      }
     }
 );
 
