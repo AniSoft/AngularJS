fd6fca29b0de8f16e248c47bdf86dcd72b62991a
Anita <anitanikolova@yahoo.com>
2015-01-08 12:53:35 +0200

    app/js/controllers/UserAdsController.js

--- Client-FINAL/app/js/controllers/UserPublishNewAdController.js
+++ Client-FINAL/app/js/controllers/UserPublishNewAdController.js
@@ -6,16 +6,18 @@ app.controller('UserPublishNewAdController',
     $scope.adData = {townId: null, categoryId: null};
     $scope.categories = categoriesService.getCategories();
     $scope.towns = townsService.getTowns();
+
     $scope.publishAd = function(adData) {
       userService.createNewAd(adData,
         function success() {
-        // TODO: show “success” info message
+          notifyService.showInfo("Publish Ad Successful");
           $location.path("/user/ads");
         },
+
         function error(err) {
-          // TODO: show error message
+          notifyService.showError("Publish Ad Failed", err);
         }
-        );
+      );
     };
 
     $scope.fileSelected = function(fileInputField) {
